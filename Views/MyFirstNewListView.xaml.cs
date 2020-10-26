6b69017088e16c2bb49a86992541aea6820105d9
Arthur PEYRARD <arthur@TurtleDebian.home>
2020-10-26 18:37:45 +0100

    Creating a view

---
+++ Views/MyFirstNewListView.xaml.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyApp.Views
+{
+    /// <summary>
+    /// Xaml & related Class auto generated with command line 'dotnet new avalonia.usercontrol -o Views -n MyFirstNewListView --namespace MyApp.Views'
+    /// </summary>
+    public class MyFirstNewListView : UserControl
+    {
+        public MyFirstNewListView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
